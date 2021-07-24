4b38f93e5e1fba0a2bde89ee14e186dc693d8b8b
Tuğra Güldoğan <tugraguldogan44@gmail.com>
2021-07-24 11:12:50 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+	---- HoloPaint ----
+	A holographic drawing app
+	Created by Tuğra Güldoğan
+	Copyright (C) 2021
+*/
+
+var express = require("express");
+var app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+app.set("port", 1337);
+app.use(express.static(__dirname + "/www"));
+app.use("/jquery", express.static(__dirname + "/node_modules/jquery/dist")); // Ugh... jQuery
+app.use("/spectrum-colorpicker", express.static(__dirname + "/node_modules/spectrum-colorpicker"));
+
+http.listen(app.get("port"), function(){
+	console.log("listening on port " + app.get("port"));
+});
+
+var clientWidth = 0;
+
+io.on("connection", function(socket){
+	console.log("connected: " + socket.handshake.address);
+
+	socket.on("disconnect", function(){
+		console.log("disconnected: " + socket.handshake.address);
+	});
+
+	socket.on("setWidth", function(width){
+		clientWidth = width;
+		socket.broadcast.emit("setWidth", clientWidth);
+		console.log("width set to " + width);
+	});
+
+	socket.on("getWidth", function(){
+		socket.emit("setWidth", clientWidth);
+	});
+
+	socket.on("draw", function(line){
+		socket.broadcast.emit("draw", line);
+	});
+});
